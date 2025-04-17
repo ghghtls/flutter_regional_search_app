c572561f6976157f8a9f56bdccfe30b165f7b89a
ghghtls <ghghtls2@gmail.com>
2025-04-17 22:42:26 +0900

    feat:홈페이지 ui 초안

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: Column());
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: textEditingController,
+          decoration: InputDecoration(
+            hintText: '검색어를 입력해 주세요',
+
+            ///컬럼내 타이틀로 변경
+            border: WidgetStateInputBorder.resolveWith((
+              Set<WidgetState> states,
+            ) {
+              if (states.contains(WidgetState.focused)) {
+                return OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.black),
+                );
+              }
+              return OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+              ); // 기본 테두리 반환
+            }),
+          ),
+        ),
+      ),
+      body: Column(
+        ///title,category,roadAddress
+      ),
+    );
   }
 }
