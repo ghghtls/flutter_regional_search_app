ec17d73037f8fd9dc5159cd0ede8570c541ade79
ghghtls <ghghtls2@gmail.com>
2025-04-17 23:08:26 +0900

    chore:모델 폴더이름 변경

---
+++ lib/data/model/location.dart
@@ -0,0 +1,14 @@
+class Location {
+  String title;
+  String category;
+  String roadAddress;
+
+  Location({
+    required this.title,
+    required this.category,
+    required this.roadAddress,
+  });
+
+  ///1. fromJson 네임드 생성자 만들기
+  //2. toJson 메서드 만들기
+}
